6785a086ec8ef950366dd9a4cbdbd09adb44ad77
baswarajuSaiganesh <ganeshsai946@gamail.com>
2024-08-31 01:17:51 +0530

    solution

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import './Logo.css'
+
+const Logo = () => (
+    <div className='container'>
+    <div className="logo-container">
+      <div className="diamond">
+        <div className="text">
+          <h1 className="title">HTML<span>&</span>CSS</h1>
+          <p>design and build websites</p>
+        </div>
+      </div>
+    </div>
+    </div>
+);
+  
+  export default Logo;
